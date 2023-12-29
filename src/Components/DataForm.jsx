2770d5fc7cda7be69fd213cd7b5c77a6f25cb884
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-29 22:40:21 +0100

    integration pages home

--- src/Components/DataForm.jsx
+++ src/Components/DataForm.jsx
@@ -1,8 +1,22 @@
 function DataForm(params) {
    return(
     <div>
-        <form action="">
-            <input type="image" />
+        <form action="" > 
+        <label for="avatar">
+             <input type="file" id="avatar" name="avatar" accept="image/png" />
+             </label> <br />
+
+            <label for="nom">
+                 votre nom :
+                <input type="text" name="nom" id="nom" />
+                </label> <br />
+
+                <label for="mail">
+                    Entrez votre adresse mail :
+                <input type="email" id="mail" name="mail"/>
+                </label>
+                <br />
+                <button>Connection </button>
         </form>
     </div>
    ); 
